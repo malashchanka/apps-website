2bf2c24ee3f292d01e69d2210dce9534d1af10f1
malashchanka <mikhail.malashchanka@gmail.com>
2024-07-20 13:47:02 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
     activeLinks.forEach(function(link) {
         link.addEventListener('click', function(event) {
             event.preventDefault();
+            console.log("Clicked on an active link, preventing default action.");
         });
     });
 
-    // Check if the current path includes 'photo-editor' path
+    // Check if the current path includes 'photo-editor'
     const path = window.location.pathname;
+    console.log("Current path:", path);
     if (path.includes('/photo-editor/')) {
         // Replace the current state so that it navigates to /apps/ on back button
+        console.log("Path includes '/photo-editor/', replacing state and pushing /apps/");
         history.replaceState({ page: path }, document.title, path);
 
         // Push the /apps/ state into the history stack
@@ -19,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Handle the back button to navigate to the correct page
     window.addEventListener("popstate", function(event) {
+        console.log("Popstate event:", event.state);
         if (event.state && event.state.page) {
             window.location.href = event.state.page;
         }
