71d1c4e7905ee8a9150fae53cc89c1fa9b907ff0
malashchanka <mikhail.malashchanka@gmail.com>
2024-07-20 13:19:07 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Welcome to My GitHub Page");
+
+document.addEventListener("DOMContentLoaded", function() {
+    const activeLinks = document.querySelectorAll('a.active');
+    activeLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+        });
+    });
+});
